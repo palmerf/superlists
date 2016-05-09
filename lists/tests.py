777bb23bfc5e49ab180a12ddf0647ed24be9ea0d
Forrest Palmer <palmerforrest@gmail.com>
2016-05-09 16:43:16 -0400

    Corrected tests.py to allow the use of the CSRF tag.

--- lists/tests.py
+++ lists/tests.py
@@ -14,8 +14,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-#        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(response.content.decode(), expected_html)
 
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
@@ -24,10 +24,15 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         expected_html = render_to_string(
             'home.html',
-            {'new_item_text': 'A new list item'}
+            {'new_item_text': 'A new list item'},
+            request=request
         )
         self.assertEqual(response.content.decode(), expected_html)
 
